6eb4f0bcafa525f44050806a0ae872967bd45684
davinchicoronado <david.coronado@mail.escuelaing.edu.co>
2021-01-31 18:11:40 -0500

    correcion

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,15 +1,31 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		PrimeFinderThread pft=new PrimeFinderThread(0, 10000000);
-		PrimeFinderThread ptf2=new PrimeFinderThread(10000001, 20000000);
-                PrimeFinderThread ptf3=new PrimeFinderThread(20000001, 30000000);
-                
-		pft.start();
-		
+                PrimeFinderThread pft2=new PrimeFinderThread(10000001, 20000000);
+                PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000);	
+                pft.start();
+                pft2.start();
+                pft3.start();
+            
+            try {
+                pft.join();
+                pft2.join();
+                pft3.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+
+
+	
+                System.out.println(Runtime.getRuntime().availableProcessors());
 		
 	}
 	
