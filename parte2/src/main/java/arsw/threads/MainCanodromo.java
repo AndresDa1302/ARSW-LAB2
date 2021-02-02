1363febdeb0b91c2f4b2519e02297d3bae805f36
davinchicoronado <david.coronado@mail.escuelaing.edu.co>
2021-02-01 23:27:50 -0500

    PuntoIII.1

--- parte2/src/main/java/arsw/threads/MainCanodromo.java
+++ parte2/src/main/java/arsw/threads/MainCanodromo.java
@@ -2,6 +2,8 @@ package arsw.threads;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 
@@ -35,10 +37,17 @@ public class MainCanodromo {
                                     //crea los hilos 'galgos'
                                     galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
                                     //inicia los hilos
-                                    galgos[i].start();
-
+                                    galgos[i].start();         
                                 }
-                               
+                                for (int i = 0; i < can.getNumCarriles(); i++) {
+                                     
+                                    try {
+                                        galgos[i].join();
+                                    } catch (InterruptedException ex) {
+                                        Logger.getLogger(MainCanodromo.class.getName()).log(Level.SEVERE, null, ex);
+                                    }
+                                    
+                                }
 				can.winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
                                 System.out.println("El ganador fue:" + reg.getGanador());
                             }
